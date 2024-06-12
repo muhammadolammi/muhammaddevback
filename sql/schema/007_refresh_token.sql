2a147f6a88b3a6f7ba6ad093b961c22db0b00c64
muhammadolammi <muhammadolammi@gmail.com>
2024-06-12 14:57:37 +0100

    adding jwt

---
+++ sql/schema/007_refresh_token.sql
@@ -0,0 +1,15 @@
+-- +goose Up 
+-- Enable the uuid-ossp extension
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+CREATE TABLE refresh_tokens (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    token TEXT UNIQUE NOT NULL ,
+    created_at TIMESTAMP NOT NULL,
+    expires_at TIMESTAMP NOT NULL,
+    user_id UUID REFERENCES users(id)  ON DELETE CASCADE NOT NULL
+
+);
+
+-- +goose Down
+DROP TABLE refresh_tokens;
+
